ea3d1b06aef2692b097e7596efd74188912a1cf2
Leo <leo.zamorano@rollingcodestudio.com>
2026-01-13 00:06:37 -0300

    fix ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'sistema-inmobiliaria',
-      script: 'yarn',
-      args: 'start',
+      script: 'node_modules/.bin/next',
+      args: 'start -p 3002',
       cwd: '/root/project-inmobiliaria',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'production',
-        PORT: 3002
+        NODE_ENV: 'production'
       }
     }
   ]
